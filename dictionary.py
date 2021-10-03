7b39d34ac1089edf41008245f9eaf959b90b649a
Aung Min Oo <aungminoo1314@gmail.com>
2021-10-03 23:38:55 +0900

    dictionay in python

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+person = {
+    'name' : 'aung min oo',
+    'age' : 20
+}
+if('name' in person) :
+    print('its worked')
+
+# print(person)
+# person['hair color'] = "black"
+
+# print('name' in person)
+
+# print(person)
+# print(person['name'])
+
+# change key to list
+# personKey = list(person.keys()) 
+
+# change value to list
+personValue = list(person.values())
+print (personValue)
