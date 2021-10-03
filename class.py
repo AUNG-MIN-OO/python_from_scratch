df8f0390589b138b4db5b5f91e388c3b2d37ef59
Aung Min Oo <aungminoo1314@gmail.com>
2021-10-04 01:08:54 +0900

    lesson 18 completed

--- class.py
+++ class.py
@@ -1,19 +1,28 @@
 # name = "aung min oo"
 # print(name.upper())
 class Car : 
-    def __init__(self,name,wheels) :
+    sterring = 1 #class level attribute
+    def __init__(self,name,wheels) : #instant level attribute
         self.name = name
         self.wheels = wheels
 
     def drive(self) :
         print(f'{self.name} is driving')
 
+    @classmethod
+    def common(cls):
+        print(f'all car have only {cls.sterring} sterring wheel')
+
 lambo = Car("lamboghini",4)
 marcedes = Car("marcedes",6)
-lambo.drive()
-print(lambo.name)
-print(lambo.wheels)
+lambo.common()
+# print(Car.common())
 
-marcedes.drive()
-print(marcedes.name)
-print(marcedes.wheels)
+# lambo.drive()
+# print(lambo.name)
+# print(lambo.wheels)
+
+# marcedes.drive()
+# print(marcedes.name)
+# print(marcedes.wheels)
+
