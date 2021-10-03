c6542845cc88d1a662444b8566a1982782bf5224
Aung Min Oo <aungminoo1314@gmail.com>
2021-10-03 14:51:48 +0900

    lesson 11 completed

---
+++ range.py
@@ -0,0 +1,8 @@
+# nums = [0,1,2,3,4]
+# for num in range(5,21,5) : 
+#     print (num)
+
+names = ['aung aung','kyaw kyaw','aung min oo','kg ma lay','2kg ma lay']
+
+for num in range(len(names)) :
+    print(names[num])
