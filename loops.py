75d908500a26de4c505038f340faceccbe317ac1
Aung Min Oo <aungminoo1314@gmail.com>
2021-10-03 14:24:32 +0900

    lesson 9 completed

---
+++ loops.py
@@ -0,0 +1,13 @@
+# names = ['aung aung','kyaw kyaw','aung min oo','kg ma lay']
+
+# for name in names :
+#     if name == 'aung min oo' :
+#         print (f'{name} is instructor')
+#         break
+#     else :
+#         print (f'{name} is student')
+
+fruits = ['apple','melon','orange']
+
+for fruit in fruits :
+    print (f'{fruit} is fruit');
