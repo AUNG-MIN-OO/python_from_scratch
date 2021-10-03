a9a15dc435b34fe771b04b5323453634a2fa132c
Aung Min Oo <aungminoo1314@gmail.com>
2021-10-03 12:49:13 +0900

    lesson 6

---
+++ formatString.py
@@ -0,0 +1,4 @@
+age = 20 ;
+# print ('my age is',age,'my birth year is',2021-age);
+
+print(f'My age is {age}. My birth year is {2021-age}.')
--- input.py
+++ input.py
@@ -2,4 +2,4 @@
 PI = 3.14;
 r = int(input('radius : '))
 area = PI*r**2;
-print("area is "+ area);
+print("area is "+ str(area));
