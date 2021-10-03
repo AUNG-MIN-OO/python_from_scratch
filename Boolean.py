c880889551022844895698123eaa73359e43fea9
Aung Min Oo <aungminoo1314@gmail.com>
2021-10-03 13:47:58 +0900

    lesson 7 complete

---
+++ Boolean.py
@@ -0,0 +1,16 @@
+# hungry = False;
+
+# print(hungry);
+
+# comparison operator === >= <= != === 
+
+# print(5<=3)
+# print(5>=3)
+# print(5==3)
+
+# logical operator
+
+# print(not True);
+# print(not False)
+# print(True and False)
+# print(False and True)
