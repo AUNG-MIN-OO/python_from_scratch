bb1775d198ab1eefb1bc14ef73e59b8232d2eb52
Aung Min Oo <aungminoo1314@gmail.com>
2021-10-04 22:32:49 +0900

    decorator completed

---
+++ decorator.py
@@ -0,0 +1,15 @@
+def greet(fun) :
+    def wrapper(name):
+        # before
+        print ('hello')
+        fun(name)
+        # after
+        print ('good bye')
+
+    return wrapper
+
+@greet
+def sayName(name)  : 
+    print (name)
+
+sayName('amo')
